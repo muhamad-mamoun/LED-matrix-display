b04246ad2075b2c0e97bc86c89542c59401a2b87
Muhamad Mamoun <muhamadmamoun@gmail.com>
2023-08-30 01:18:06 +0300

    Update the Display module

--- APP/display.c
+++ APP/display.c
@@ -1,10 +1,10 @@
 /*
 ====================================================================================================================
 Author       : Mamoun
-Module       : 
+Module       : Display
 File Name    : display.c
 Date Created : Aug 11, 2023
-Description  : 
+Description  : Source file for the Display driver [Used to display on the LED Matrix].
 ====================================================================================================================
 */
 
@@ -16,6 +16,7 @@ Description  :
 #include "display.h"
 #include "characters_map.h"
 #include "../HAL/shift_register.h"
+#include "../MCAL/gpio.h"
 #include <util/delay.h>
 
 /*==================================================================================================================
@@ -25,7 +26,7 @@ Description  :
 static HC595_registerPinsType g_rowsRegisterPins = {PORTA_ID,PIN0_ID,PORTA_ID,PIN1_ID,PORTA_ID,PIN2_ID};
 static HC595_registerPinsType g_colsRegisterPins = {PORTA_ID,PIN3_ID,PORTA_ID,PIN4_ID,PORTA_ID,PIN5_ID};
 
-DISPLAY_characterMapType g_display_buffer[DISPLAY_BUFFER_SIZE] = {0};
+static DISPLAY_characterMapIndexType g_display_buffer[DISPLAY_BUFFER_SIZE] = {0};
 
 /*==================================================================================================================
                                       < Private Functions Prototypes >
@@ -49,7 +50,7 @@ static void DISPLAY_shiftBuffer(void);
  *                   <>      ->
  * [return]        : The function returns void.
  =================================================================================================================*/
-static void DISPLAY_buffer(void);
+static void DISPLAY_renderBuffer(void);
 
 
 /*==================================================================================================================
@@ -59,78 +60,95 @@ static void DISPLAY_buffer(void);
  *                   <>      ->
  * [return]        : The function returns void.
  =================================================================================================================*/
-static void DISPLAY_spaceBetweenCharacters(void);
+static void DISPLAY_emptyColumn(uint8 a_numberOfColumns);
 
 /*==================================================================================================================
                                           < Functions Definitions >
 ==================================================================================================================*/
 
 /*==================================================================================================================
- * [Function Name] : 
- * [Description]   : 
- * [Arguments]     : <>      -> 
- *                   <>      -> 
+ * [Function Name] : DISPLAY_init
+ * [Description]   : Initialize the Display module by setting up the rows and columns shift registers.
+ * [Arguments]     : The function takes no arguments.
  * [return]        : The function returns void.
  =================================================================================================================*/
 void DISPLAY_init(void)
 {
-	HC595_init(&g_rowsRegisterPins);                            /* Initialize rows shift register.     */
-	HC595_init(&g_colsRegisterPins);                            /* Initialize columnss shift register. */
+	HC595_init(&g_rowsRegisterPins);                            /* Initialize the rows shift register.     */
+	HC595_init(&g_colsRegisterPins);                            /* Initialize the columnss shift register. */
 }
 
+/*==================================================================================================================
+ * [Function Name] : DISPLAY_string
+ * [Description]   : Display a specific string on the LED Matrix screen.
+ * [Arguments]     : <a_ptr2message>      -> Pointer points to the required string to be displayed.
+ * [return]        : The function returns void.
+ =================================================================================================================*/
 void DISPLAY_string(DISPLAY_characterType* a_ptr2message)
 {
 	for(uint8 character_counter = 0; *(a_ptr2message + character_counter) != MESSAGE_TERMINATOR; character_counter++)
 	{
-		DISPLAY_characterMapType* character_map_address = (DISPLAY_characterMapType*)(g_character_map + *(a_ptr2message + character_counter) - 32);
-		for(uint8 character_map_index = 0; *(character_map_address + character_map_index) != 0XFF; character_map_index++)
+		DISPLAY_characterMapIndexType* character_map_address = CALCULATE_CHARACTER_MAP_ADDRESS(*(a_ptr2message + character_counter));
+		for(uint8 character_map_index = 0; *(character_map_address + character_map_index) != CHARACTER_MAP_TERMINATOR; character_map_index++)
 		{
 			DISPLAY_shiftBuffer();
 			g_display_buffer[0] = *(character_map_address + character_map_index);
-			DISPLAY_buffer();
+			DISPLAY_renderBuffer();
 		}
-		DISPLAY_spaceBetweenCharacters();
+		DISPLAY_emptyColumn(2);
 	}
 	/* display ~2 spaces before display it again. */
 }
 
+static void DISPLAY_emptyColumn(uint8 a_numberOfColumns)
+{
+	for(uint8 space_counter = 0; space_counter < a_numberOfColumns; space_counter++)
+	{
+		DISPLAY_shiftBuffer();
+		g_display_buffer[0] = 0X00;
+		DISPLAY_renderBuffer();
+	}
+}
+
 /*==================================================================================================================
- * [Function Name] :
- * [Description]   :
- * [Arguments]     : <>      ->
- *                   <>      ->
+ * [Function Name] : DISPLAY_shiftBuffer
+ * [Description]   : Shift the entire Buffer array by one index.
+ * [Arguments]     : The function takes no arguments.
  * [return]        : The function returns void.
  =================================================================================================================*/
 static void DISPLAY_shiftBuffer(void)
 {
 	for(sint8 counter = (DISPLAY_BUFFER_SIZE - 2); counter >= 0; counter--)
 	{
+		/* Store each index in the array at the next index of it. */
 		g_display_buffer[counter + 1] = g_display_buffer[counter];
 	}
 }
 
-static void DISPLAY_buffer(void)
+/*==================================================================================================================
+ * [Function Name] : DISPLAY_renderBuffer
+ * [Description]   : Display the content of the Buffer on the LED Matrix screen.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ =================================================================================================================*/
+static void DISPLAY_renderBuffer(void)
 {
-	for(uint8 iteration_counter = 0; iteration_counter < 2; iteration_counter++)
+	for(uint8 iteration_counter = 0; iteration_counter < DISPLAY_SPEED; iteration_counter++)
 	{
-		/* Activate the first column. */
+		/* Activate the first column in the matrix. */
 		HC595_updatedShiftRegister(&g_colsRegisterPins,1,1);
-		for(uint8 counter = 0; counter < NUMBER_OF_COLS; counter++)
+
+		/* Round-Robin on the entire columns in the matrix one by one. */
+		for(uint8 column_counter = 0; column_counter < SCREEN_NUMBER_OF_COLS; column_counter++)
 		{
-			/* fill each column with delay */
-			HC595_updatedShiftRegister(&g_rowsRegisterPins,NUMBER_OF_ROWS,*(g_display_buffer + counter));
-			_delay_us(500);
+			/* Fill the current column with its value by shifting the row register. */
+			HC595_updatedShiftRegister(&g_rowsRegisterPins,SCREEN_NUMBER_OF_ROWS,*(g_display_buffer + column_counter));
+
+			/* Small delay the eye cannot perceive it, So it appears continuously. */
+			_delay_us(ITERATION_SPEED);
+
+			/* Activate the next column in the matrix. */
 			HC595_updatedShiftRegister(&g_colsRegisterPins,1,0);
 		}
-	}
-}
-
-static void DISPLAY_spaceBetweenCharacters(void)
-{
-	for(uint8 space_counter = 0; space_counter < 2; space_counter++)
-	{
-		DISPLAY_shiftBuffer();
-		g_display_buffer[0] = 0X00;
-		DISPLAY_buffer();
 	}
 }
--- APP/display.h
+++ APP/display.h
@@ -4,7 +4,7 @@ Author       : Mamoun
 Module       : Display
 File Name    : display.h
 Date Created : Aug 11, 2023
-Description  : 
+Description  : Header file for the Display driver [Used to display on the LED Matrix].
 =====================================================================================================================
 */
 
@@ -16,40 +16,43 @@ Description  :
                                                < Includes >
 ===================================================================================================================*/
 
-#include "../MCAL/gpio.h"
 #include "../OTHERS/std_types.h"
 
 /*===================================================================================================================
                                 < Definitions and Static Configurations >
 ===================================================================================================================*/
 
-#define NUMBER_OF_ROWS                     (8U)
-#define NUMBER_OF_COLS                     (32U)
-#define DISPLAY_BUFFER_SIZE                (100U)
-#define MESSAGE_TERMINATOR                 ('\0')
+#define SCREEN_NUMBER_OF_ROWS                     (8U)
+#define SCREEN_NUMBER_OF_COLS                     (32U)
+#define DISPLAY_BUFFER_SIZE                       (100U)
+#define MESSAGE_TERMINATOR                        ('\0')
+#define ITERATION_SPEED                           (500)
+#define DISPLAY_SPEED                             (2U)    /* The lower the value, the faster the display.  */
 
 #if (DISPLAY_BUFFER_SIZE > 127)
 #error "Display buffer size limit is 127." /* To increase it, you need to change some variables datatypes. */
 #endif
 
-/*===================================================================================================================
-                                       < Shared Global Variables >
-===================================================================================================================*/
+#if (DISPLAY_SPEED <= 0)
+#error "Invalid display speed."
+#endif
 
-
+#if (DISPLAY_SPEED > 20)
+#warning "The display speed is too slow."
+#endif
 
 /*===================================================================================================================
                                        < User-defined Data Types >
 ===================================================================================================================*/
 
 typedef unsigned char DISPLAY_characterType;
-typedef unsigned char DISPLAY_characterMapType;
+typedef unsigned char DISPLAY_characterMapIndexType;
 
 /*===================================================================================================================
                                          < Function-like Macros >
 ===================================================================================================================*/
 
-
+#define CALCULATE_CHARACTER_MAP_ADDRESS(CHARACTER) ((DISPLAY_characterMapIndexType*)((g_character_map) + (CHARACTER) - (FIRST_CHARACTER_VALUE)))
 
 /*===================================================================================================================
                                          < Functions Prototypes >
@@ -59,8 +62,7 @@ typedef unsigned char DISPLAY_characterMapType;
 /*==================================================================================================================
  * [Function Name] :
  * [Description]   :
- * [Arguments]     : <>      ->
- *                   <>      ->
+ * [Arguments]     : The function takes no arguments.
  * [return]        : The function returns void.
  =================================================================================================================*/
 void DISPLAY_init(void);
